9423bd8731880fa2c2471b56477aaf27722a7f51
ysp <54001533+ysp313@users.noreply.github.com>
2019-10-14 12:28:25 +0200

    Update Indianna.java

--- Javaprojects/Variables/Indianna.java
+++ Javaprojects/Variables/Indianna.java
@@ -4,7 +4,7 @@ class  Indianna {
 		String title = "The last crusade";
 		Boolean vu = true;
 		int date = 1989;
-		int score = 65;
+		float score = 8.2;
 		// afficher résumé
 	   	System.out.println(title+ " Le film a été vu? "+ vu+ " Année de production:"+ date+ " Metascore:"+ score);
 	}
