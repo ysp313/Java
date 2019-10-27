f64f20af6803539b49a191621b4f095eaefe2e66
Yves PIQUET <Yves@MacBook-ysp.local>
2019-10-27 10:43:35 +0100

    add files

---
+++ Javaprojects/quest-java-poo3/Eagle.java
@@ -0,0 +1,24 @@
+public class Eagle extends Bird {
+
+    private boolean flying;
+    private int altitude;
+
+    public Eagle(String name) {
+        super(name);
+        this.flying = false;
+        this.altitude = 0;
+    }
+
+    public int getAltitude() {
+        return altitude;
+    }
+
+    public boolean isFlying() {
+        return flying;
+    }
+
+    @Override
+    public String sing() {
+        return "Screech!";
+    }
+}
