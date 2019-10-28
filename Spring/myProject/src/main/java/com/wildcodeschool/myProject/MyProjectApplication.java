8335818dcc768e8493bb795f6c3f057f1436e004
Yves PIQUET <Yves@MacBook-ysp.lan>
2019-10-28 10:34:38 +0100

    Update

--- Spring/myProject/src/main/java/com/wildcodeschool/myProject/MyProjectApplication.java
+++ Spring/myProject/src/main/java/com/wildcodeschool/myProject/MyProjectApplication.java
@@ -2,12 +2,22 @@ package com.wildcodeschool.myProject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 @SpringBootApplication
 public class MyProjectApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MyProjectApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MyProjectApplication.class, args);
+    }
+
+    @RequestMapping("/")
+    @ResponseBody
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
 
 }
