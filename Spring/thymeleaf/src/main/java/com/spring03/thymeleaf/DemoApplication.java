38ecce0544fe5094e9610cd15655e499c15e4f82
Yves PIQUET <Yves@MacBook-ysp.local>
2019-11-03 20:52:09 +0100

    ADD

---
+++ Spring/thymeleaf/src/main/java/com/spring03/thymeleaf/DemoApplication.java
@@ -0,0 +1,39 @@
+package com.spring03.thymeleaf;
+
+import org.springframework.ui.Model;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@GetMapping("/doctor")
+	public String doctor(Model model,
+						@RequestParam(value="name",
+								required=false,
+								defaultValue="John Smith") String name ,
+						@RequestParam(value="number",
+								required=false,
+								defaultValue="01") int number) 
+		// une fois les paramètres de la methode complétés, on ajoute les attributs
+		{
+			model.addAttribute("name", name);
+			model.addAttribute("number", number);
+			return "doctor"; // renvoi au template "doctor.html"
+		}
+		//optionnel
+		@GetMapping("/")
+			public String index()
+			{
+				return "index";
+			}
+
+}
