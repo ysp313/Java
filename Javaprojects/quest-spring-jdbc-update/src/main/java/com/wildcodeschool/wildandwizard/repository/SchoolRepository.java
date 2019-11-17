1ae1fcb64fb90cf9a03e7df525f66b03b15195f2
ysp <54001533+ysp313@users.noreply.github.com>
2019-11-17 13:36:57 +0100

    Final

--- Javaprojects/quest-spring-jdbc-update/src/main/java/com/wildcodeschool/wildandwizard/repository/SchoolRepository.java
+++ Javaprojects/quest-spring-jdbc-update/src/main/java/com/wildcodeschool/wildandwizard/repository/SchoolRepository.java
@@ -12,7 +12,28 @@ public class SchoolRepository {
 
     public School update(Long id, String name, Long capacity, String country) {
 
-        // TODO : update a school from the database
+        // Requete de connection à la bd.
+        try {
+            Connection connection = DriverManager.getConnection(
+                    DB_URL, DB_USER, DB_PASSWORD
+            );
+            PreparedStatement statement = connection.prepareStatement(
+                    "UPDATE school SET name=?, capacity=?, country=? WHERE id=?"
+            );
+            statement.setString(1, name);
+            statement.setLong(2, capacity);
+            statement.setString(3, country);
+            statement.setLong(4, id);
+
+            if (statement.executeUpdate() != 1) {
+                throw new SQLException("failed to update data");
+            }
+
+            // Renvoi d'un objet ecole avec ses paramètres.
+            return new School(id, name, capacity, country);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
