586a7a4cd23133650dc4ea67456734dfd8b32cb0
ysp <54001533+ysp313@users.noreply.github.com>
2019-11-17 13:58:18 +0100

    final

--- Javaprojects/quest-spring-jdbc-delete/src/main/java/com/wildcodeschool/wildandwizard/repository/SchoolRepository.java
+++ Javaprojects/quest-spring-jdbc-delete/src/main/java/com/wildcodeschool/wildandwizard/repository/SchoolRepository.java
@@ -13,7 +13,22 @@ public class SchoolRepository {
     private final static String DB_PASSWORD = "Horcrux4life!";
 
     public void deleteById(Long id) {
-        // TODO: delete a school from the database
+        // Connection à la DB
+        try {
+            Connection connection = DriverManager.getConnection(
+                    DB_URL, DB_USER, DB_PASSWORD
+            );
+            PreparedStatement statement = connection.prepareStatement(
+                    "DELETE FROM school WHERE id=?"
+            );
+            statement.setLong(1, id);
+
+            if (statement.executeUpdate() != 1) {
+                throw new SQLException("failed to delete data");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public List<School> findAll() {
