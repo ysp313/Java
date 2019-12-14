e7c06c898faeb40a632ec86864a81faec604ed36
ysp <54001533+ysp313@users.noreply.github.com>
2019-12-14 11:30:12 +0100

    some changes like version and controller

--- Javaprojects/spring-swapi-master/src/main/java/com/wildcodeschool/swapi/controller/SwapiController.java
+++ Javaprojects/spring-swapi-master/src/main/java/com/wildcodeschool/swapi/controller/SwapiController.java
@@ -21,11 +21,25 @@ public class SwapiController {
         return "index";
     }
 
-    @GetMapping("/planet")
+    @GetMapping("/planets")
     public String planet(Model model, @RequestParam Long id) {
 
+        WebClient webClient = WebClient.create(SWAPI_URL);
+        Mono<String> call = webClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/planets/{id}/")
+                        .build(id))
+                .retrieve()
+                .bodyToMono(String.class);
+
+        String response = call.block();
+        ObjectMapper objectMapper = new ObjectMapper();
         Planet planetObject = null;
-        // TODO : call the API and retrieve the planet
+        try {
+            planetObject = objectMapper.readValue(response, Planet.class);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
 
         model.addAttribute("planetInfos", planetObject);
 
--- Javaprojects/spring-swapi-master/src/main/java/com/wildcodeschool/swapi/model/Planet.java
+++ Javaprojects/spring-swapi-master/src/main/java/com/wildcodeschool/swapi/model/Planet.java
@@ -4,10 +4,131 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class Planet {
 
-    // TODO : add attributes
+    private String name;
+    @JsonProperty("rotation_period")
+    private String rotationPeriod;
+    @JsonProperty("orbital_period")
+    private String orbitalPeriod;
+    private String diameter;
+    private String climate;
+    private String gravity;
+    @JsonProperty("surface_water")
+    private String surfaceWater;
+    private String population;
+    private String[] resident;
+    private String[] film;
+    private String created;
+    private String edited;
+    private String url;
+	
 
-    // TODO : add an empty constructor
+    public Planet() {
+    }
 
-    // TODO : add getters and setters
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRotationPeriod() {
+        return this.rotationPeriod;
+    }
+
+    public void setRotationPeriod(String rotationPeriod) {
+        this.rotationPeriod = rotationPeriod;
+    }
+
+    public String getOrbitalPeriod() {
+        return this.orbitalPeriod;
+    }
+
+    public void setOrbitalPeriod(String orbitalPeriod) {
+        this.orbitalPeriod = orbitalPeriod;
+    }
+
+    public String getDiameter() {
+        return this.diameter;
+    }
+
+    public void setDiameter(String diameter) {
+        this.diameter = diameter;
+    }
+
+    public String getClimate() {
+        return this.climate;
+    }
+
+    public void setClimate(String climate) {
+        this.climate = climate;
+    }
+
+    public String getGravity() {
+        return this.gravity;
+    }
+
+    public void setGravity(String gravity) {
+        this.gravity = gravity;
+    }
+
+    public String getSurfaceWater() {
+        return this.surfaceWater;
+    }
+
+    public void setSurfaceWater(String surfaceWater) {
+        this.surfaceWater = surfaceWater;
+    }
+
+    public String getPopulation() {
+        return this.population;
+    }
+
+    public void setPopulation(String population) {
+        this.population = population;
+    }
+
+    public String[] getResident() {
+        return resident;
+    }
+
+    public void setResident(String[] resident) {
+        this.resident = resident;
+    }
+
+    public String[] getFilm() {
+        return film;
+    }
+
+    public void setFilm(String[] film) {
+        this.film = film;
+    }
+
+    public String getCreated() {
+        return this.created;
+    }
+
+    public void setCreated(String created) {
+        this.created = created;
+    }
+
+    public String getEdited() {
+        return this.edited;
+    }
+
+    public void setEdited(String edited) {
+        this.edited = edited;
+    }
+
+    public String getUrl() {
+        return this.url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
 
 }
