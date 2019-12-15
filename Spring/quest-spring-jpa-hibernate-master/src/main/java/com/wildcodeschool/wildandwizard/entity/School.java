58330be6838411bab72a6a4ac877c7594f371fbc
ysp <54001533+ysp313@users.noreply.github.com>
2019-12-15 18:28:50 +0100

    add hibernate

---
+++ Spring/quest-spring-jpa-hibernate-master/src/main/java/com/wildcodeschool/wildandwizard/entity/School.java
@@ -0,0 +1,46 @@
+package com.wildcodeschool.wildandwizard.entity;
+
+// TODO : update this entity
+
+public class School {
+
+    private Long id;
+    private String name;
+    private Long capacity;
+    private String country;
+
+    public School() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(Long capacity) {
+        this.capacity = capacity;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+}
