86c88d4bebd2cb84432194188fec5df494a46b57
ysp313 <jarryves07@gmail.com>
2019-12-15 19:51:58 +0100

    finish

--- Spring/quest-spring-jpa-hibernate-master/src/main/java/com/wildcodeschool/wildandwizard/controller/SchoolController.java
+++ Spring/quest-spring-jpa-hibernate-master/src/main/java/com/wildcodeschool/wildandwizard/controller/SchoolController.java
@@ -1,6 +1,9 @@
 package com.wildcodeschool.wildandwizard.controller;
 
+import java.util.Optional;
 import com.wildcodeschool.wildandwizard.entity.School;
+import com.wildcodeschool.wildandwizard.repository.SchoolRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,12 +14,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class SchoolController {
 
-    // TODO : get school repository by dependency injection
+    @Autowired
+    private SchoolRepository repository;
 
     @GetMapping("/schools")
     public String getAll(Model model) {
 
-        // TODO : find all schools
+        model.addAttribute("schools", repository.findAll());
 
         return "schools";
     }
@@ -25,7 +29,14 @@ public class SchoolController {
     public String getSchool(Model model,
                             @RequestParam(required = false) Long id) {
 
-        // TODO : find one school by id
+        School school = new School();
+        if (id != null){
+            Optional<School> optionalSchool = repository.findById(id);
+            if (optionalSchool.isPresent()){
+                school = optionalSchool.get();
+            }
+        }
+            model.addAttribute("school", school);
 
         return "school";
     }
@@ -33,15 +44,14 @@ public class SchoolController {
     @PostMapping("/school")
     public String postSchool(@ModelAttribute School school) {
 
-        // TODO : create or update a school
-
+        repository.save(school);
         return "redirect:/schools";
     }
 
     @GetMapping("/school/delete")
     public String deleteSchool(@RequestParam Long id) {
 
-        // TODO : delete a school
+        repository.deleteById(id);
 
         return "redirect:/schools";
     }
--- Spring/quest-spring-jpa-hibernate-master/src/main/java/com/wildcodeschool/wildandwizard/controller/WizardController.java
+++ Spring/quest-spring-jpa-hibernate-master/src/main/java/com/wildcodeschool/wildandwizard/controller/WizardController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
 import java.util.Optional;
 
 @Controller
