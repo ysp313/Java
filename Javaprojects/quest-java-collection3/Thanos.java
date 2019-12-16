7e5a67e80d9e9285245a53b6259529c71e7e2a66
ysp313 <jarryves07@gmail.com>
2019-12-16 09:54:26 +0100

    finish

--- Javaprojects/quest-java-collection3/Thanos.java
+++ Javaprojects/quest-java-collection3/Thanos.java
@@ -14,14 +14,14 @@ public class Thanos {
         heroes.add(new Hero("Doctor Strange", 42));
 
         // TODO 1 : Retrieve and remove the oldest hero in one line
-        Hero oldest = null;
+        Hero oldest = heroes.pollLast();
 
         System.out.println("\nOldest hero:");
         System.out.println(oldest == null ? "" : oldest.getName());
 
         // TODO 2: Show heroes by age (descending)
         System.out.println("\nHeroes by age (descending) :");
-        for (Hero hero : heroes) {
+        for (Hero hero : heroes.descendingSet()) {
             System.out.println(hero.getName() + ", " + hero.getAge());
         }
 
@@ -32,7 +32,7 @@ public class Thanos {
         heroes.add(ironman);
 
         System.out.println("\nSubset of heroes :");
-        for (Hero hero : heroes) {
+        for (Hero hero : heroes.subSet(spiderman, true, ironman, true)) {
             System.out.println(hero.getName() + ", " + hero.getAge());
         }
     }
