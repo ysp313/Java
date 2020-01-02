feab85ad0877d8e283a36880f8ca9e1bb8165e3b
ysp313 <jarryves07@gmail.com>
2020-01-02 16:09:02 +0100

    add collection4

---
+++ Javaprojects/quest-java-collection4/Thanos.java
@@ -0,0 +1,30 @@
+public class Thanos {
+
+    public static void main(String[] args) {
+
+        Hero hulk = new Hero("Hulk", 49);
+        Hero blackWidow = new Hero("Black Widow", 34);
+        Hero captainAmerica = new Hero("Captain America", 100);
+        Hero thor = new Hero("Thor", 1501);
+        Hero vision = new Hero("Vision", 3);
+        Hero scarletWitch = new Hero("Scarlet Witch", 29);
+        Hero doctorStrange = new Hero("Doctor Strange", 42);
+        
+        Flower lily = new Flower("Lily");
+        Flower tulip = new Flower("Tulip");
+        Flower begonia = new Flower("Begonia");
+        Flower violet = new Flower("Violet");
+        Flower rose = new Flower("Rose");
+
+        // TODO 1 : Create a TreeMap where :
+        // Hulk, Thor and Scarlet Witch have a Rose
+        // Vision has a Tulip
+        // Captain America has a Lily
+        // Doctor Strange and Black Widow have a Violet
+
+        // TODO 2 : Print if `begonia` is contained in the TreeMap
+
+        // TODO 3 : For each hero, alphabetically, print the corresponding flower
+
+    }
+}
