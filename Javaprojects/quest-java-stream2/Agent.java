4dc69a8b94c7fe17af0af3b0a31f8d3ce8384af9
ysp <54001533+ysp313@users.noreply.github.com>
2020-02-09 09:25:17 +0200

    add folder

---
+++ Javaprojects/quest-java-stream2/Agent.java
@@ -0,0 +1,26 @@
+public class Agent {
+
+    private String firstName;
+    private String lastName;
+
+    public Agent(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
---
+++ Javaprojects/quest-java-stream2/Shield.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Shield {
+
+    public static void main(String[] args) {
+
+        List<String> names = new ArrayList<>();
+        names.add("Phil Coulson");
+        names.add("Melinda May");
+        names.add("Daisy Johnson");
+        names.add("Leopold Fitz");
+        names.add("Jemma Simmons");
+
+        // TODO 1 : map names to agents list
+        List<Agent> agents = new ArrayList<>();
+
+        showAgents(agents);
+    }
+
+    // don't touch anything here !
+    private static void showAgents(List<Agent> agents) {
+        System.out.println("\nAgents:");
+        for (Agent agent : agents) {
+            System.out.println(agent.getLastName() + " " + agent.getFirstName());
+        }
+    }
+}
