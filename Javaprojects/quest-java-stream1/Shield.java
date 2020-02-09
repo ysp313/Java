9db3baaa2a968111bb8f0955b109b9d6029d7ee2
ysp <54001533+ysp313@users.noreply.github.com>
2020-02-09 08:58:22 +0200

    Update Shield.java

--- Javaprojects/quest-java-stream1/Shield.java
+++ Javaprojects/quest-java-stream1/Shield.java
@@ -17,16 +17,22 @@ public class Shield {
         heroes.add(new Hero("Hulk", 49, true));
         heroes.add(new Hero("Doctor Strange", 42, false));
 
-        List<Hero> elders;
+        List<Hero> elders = heroes
         // TODO 1 : filter heroes in order to found heroes older than 59
+            .stream()
+            .filter(c -> c.getAge() > 59)
+            .collect(Collectors.toList());
 
         System.out.println("\nElders:");
         for (Hero elder : elders) {
             System.out.println(elder.getName());
         }
 
-        List<Hero> intolerants;
+        List<Hero> intolerants = heroes
         // TODO 2 : filter heroes in order to found heroes that are gluten intolerants
+        .stream()
+            .filter(c -> c.isGlutenIntolerant())
+            .collect(Collectors.toList());
 
         System.out.println("\nGluten intolerants:");
         for (Hero intolerant : intolerants) {
