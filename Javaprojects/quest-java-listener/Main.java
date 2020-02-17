d463fc350d73e1c2a9ab0a5790e4c7e1abb8f570
ysp <54001533+ysp313@users.noreply.github.com>
2020-02-17 09:15:09 +0100

    final version

--- Javaprojects/quest-java-listener/Main.java
+++ Javaprojects/quest-java-listener/Main.java
@@ -5,8 +5,23 @@ public class Main {
         Bank account = new Bank(1000);
 
         // TODO : create and use a TransactionListener interface with onComplete method
-        Transactions.withdraw(account, 100);
 
-        System.out.printf("Your balance is: %d%n", account.getTotal());
+        
+        Transactions.withdraw(account, 100, new TransactionListener(){
+        
+            @Override
+            public void onSuccess(Bank bank) {
+                System.out.printf("Your balance is: %d%n", account.getTotal());            }
+        
+            @Override
+            public void onLoading() {
+                System.out.println("Please wait...");                
+            }
+        
+            @Override
+            public void onError(String error) {
+                System.out.printf("Sign in error : %s%n", error);                
+            }
+        });
     }
 }
--- Javaprojects/quest-java-listener/Transactions.java
+++ Javaprojects/quest-java-listener/Transactions.java
@@ -3,12 +3,22 @@ import java.util.TimerTask;
 
 public class Transactions {
 
-    public static void withdraw(Bank account, int value) {
+    public static void withdraw(Bank account, int value,  TransactionListener event) {
+        // Le loading démarre
+        event.onLoading();
+
+        if (account == null) {
+            // Envoi du message et stop la procédure
+            event.onError("Name is null or empty");
+            return;
+        }
+
         int time = (int) (Math.random() * ((1000 - 500) + 1)) + 500;
         new Timer().schedule(new TimerTask() {
             @Override
             public void run() {
                 account.setTotal(account.getTotal() - value);
+                event.onSuccess(account);
             }
         }, time);
     }
